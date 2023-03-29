147c8bf7a7c795bb505fe1422c759f7a99486bfc
Daniel Petry <dpetry@anaconda.com>
2023-03-29 13:53:15 +0200

    Try large mcmodel...

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,7 +53,7 @@ export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,--as-needed//g')"
 export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')"
 export LDFLAGS_LD="$(echo $LDFLAGS_LD | sed 's/-dead_strip_dylibs//g')"
 if [[ "${build_platform}" = "linux-ppc64le" ]]; then
-    export CFLAGS="${CFLAGS} -mcmodel=medium"
+    export CFLAGS="${CFLAGS} -mcmodel=large"
 fi
 
 # Dynamic libraries need to be lazily loaded so that torch can be imported on
