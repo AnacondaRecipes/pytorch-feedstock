c65c51b81f15d2451c83531a58019b9feffe83f3
Daniel Petry <dpetry@anaconda.com>
2023-03-31 12:30:20 +0200

    Try turning on garbage collection for the linker

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,8 +53,8 @@ export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,--as-needed//g')"
 export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')"
 export LDFLAGS_LD="$(echo $LDFLAGS_LD | sed 's/-dead_strip_dylibs//g')"
 if [[ "${build_platform}" = "linux-ppc64le" ]]; then
-    export CFLAGS="${CFLAGS} -mlongcall -Wno-ignored-attributes"
-    export CXXFLAGS="${CXXFLAGS} -mlongcall -Wno-ignored-attributes"
+    export LDFLAGS="${LDFLAGS} -Wl,--gc-sections"
+    export LDFLAGS_LD="${LDFLAGS_LD} -Wl,--gc-sections"
 fi
 
 # Dynamic libraries need to be lazily loaded so that torch can be imported on
