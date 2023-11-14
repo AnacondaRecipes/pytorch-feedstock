ccbd36f44d47482c847fdeb66cbe10474c3d0594
danpetry <dpetry@anaconda.com>
2023-11-14 15:30:19 -0600

    Update recipe/build_pytorch.sh

--- recipe/build_pytorch.sh
+++ recipe/build_pytorch.sh
@@ -140,7 +140,7 @@ if [[ ${pytorch_variant} = "gpu" ]]; then
         elif [[ ${cudatoolkit} == 11.1 ]]; then
             export TORCH_CUDA_ARCH_LIST="3.5;3.7;5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6+PTX"
         elif [[ ${cudatoolkit} == 11.2 ]]; then
-            export TORCH_CUDA_ARCH_LIST="3.5;3.7;5.0;5.2;5.3;6.0;6.1;6.2;7.0;7.2;7.5;8.0;8.6+PTX"
+            export TORCH_CUDA_ARCH_LIST="3.5+PTX;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
         elif [[ ${cudatoolkit} == 11.3 ]]; then
             export TORCH_CUDA_ARCH_LIST="3.5+PTX;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
         elif [[ ${cudatoolkit} == 11.8 ]]; then
