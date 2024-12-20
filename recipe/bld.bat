13208087f110394097ff058895b0d44ed97b5e09
Eric Lundby <Eric.Lundby@gmail.com>
2024-12-19 19:26:07 -0700

    Use stricter sed commands
    To not mangle timestamps.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -37,8 +37,21 @@ if "%PKG_NAME%" == "pytorch" (
   :: We build libtorch for a specific python version. 
   :: This ensures its only build once. However, when that version changes 
   :: we need to make sure to update that here.
-  sed "s/3.12/%PY_VER%/g" build/CMakeCache.txt.orig > build/CMakeCache.txt
-  sed -i "s/312/%CONDA_PY%/g" build/CMakeCache.txt
+  :: Get the full python version string
+  for /f "tokens=2" %%a in ('python --version 2^>^&1') do set PY_VERSION_FULL=%%a
+
+  :: Replace Python312 or python312 with ie Python311 or python311
+  sed "s/\([Pp]ython\)312/\1%CONDA_PY%/g" original.txt > output.txt
+
+  :: Replace version string v3.12.8() with ie v3.11.11()
+  sed -i.bak -E "s/v3\.12\.[0-9]+/v%PY_VERSION_FULL%/g" output.txt
+
+  :: Replace interpreter properties Python;3;12;8;64 with ie Python;3;11;11;64
+  :: sed -i.bak -E "s/Python;3;12;[0-9]+;64/Python;%PY_VERSION_FULL%//./;};64/g" output.txt
+  sed -i.bak -E "s/Python;3;12;[0-9]+;64/Python;%PY_VERSION_FULL:.=;%;64/g" output.txt
+
+  :: Replace cp312-win_amd64 with ie cp311-win_amd64
+  sed -i.bak "s/cp312/cp311/g" output.txt
 
   :: We use a fan-out build to avoid the long rebuild of libtorch
   :: However, the location of the numpy headers changes between python 3.8
